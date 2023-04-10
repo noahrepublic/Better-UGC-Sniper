d9d3a2fa4dce27d41042364a8bc88a00b98446ee
Noah <noahdevelopsthings@gmail.com>
2023-04-10 18:11:14 -0300

    Start function

--- configeditor.bat
+++ configeditor.bat
@@ -6,12 +6,19 @@ title UGC-Sniper Config Editor by noahrepublic#4323
 echo option select
 echo.
 echo [1] - Configure Sniper (REQUIRED SET UP TO USE)
+echo [2] - Start Sniper (PYTHON REQUIRED)
 
 set /p o=
 if %o%==1 goto config
+if %o%==2 goto start
 
 pause
 
 :config
 cls
 python3 ./scripts/config.py
+
+:start
+cls
+python3 pip install -r requirements.txt
+python3 main.py
