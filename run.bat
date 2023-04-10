bbab60ab1af88bc785ea4396e2aadfc883a9c532
Noah <noahdevelopsthings@gmail.com>
2023-04-10 19:05:29 -0300

    Added config settings

--- run.bat
+++ run.bat
@@ -5,20 +5,25 @@ cls
 title UGC-Sniper Config Editor by noahrepublic#4323
 echo option select
 echo.
-echo [1] - Configure Sniper (REQUIRED SET UP TO USE)
-echo [2] - Start Sniper (PYTHON REQUIRED)
+echo [1] - Configure Sniper
+echo [2] - Add limited 
+echo [3] - Start Sniper
 
 set /p o=
 if %o%==1 goto config
-if %o%==2 goto start
+if %o%==2 goto add
+if %o%==3 goto start
 
 pause
 
 :config
 cls
 python3 ./scripts/config.py
+goto ui
+
 
 :start
 cls
 python3 pip install -r requirements.txt
 python3 main.py
+goto ui
--- scripts/config.py
+++ scripts/config.py
@@ -1,29 +1,77 @@
 import requests as r
 import json
+import os
 
+if not os.path.exists("./config.json"):
+    print("Welcome to first time setup")
+    config = {
+        "ROBLOSECURITY": "",
+        "cooldownPerLimited": 0.75,
+    }
 
-config = {
-    "ROBLOSECURITY": "",
-    "cooldownPerLimited": 0.75
-}
+    roblosecurity = str(input("Enter ROBLOSECURITY: "))
+    try:
+        userId = r.get("https://users.roblox.com/v1/users/authenticated",
+                    cookies={".ROBLOSECURITY": roblosecurity}).json()["id"]
+    except:
+        print("ROBLOSECURITY is invalid. Restart the script and try again.")
+        input("Press enter to exit...")
+        exit(0)
 
-roblosecurity = str(input("Enter ROBLOSECURITY: "))
-try:
-    userId = r.get("https://users.roblox.com/v1/users/authenticated",
-                cookies={".ROBLOSECURITY": roblosecurity}).json()["id"]
-except:
-    print("ROBLOSECURITY is invalid. Restart the script and try again.")
+        
+    config["ROBLOSECURITY"] = roblosecurity
+
+    cooldownPerLimited = input("Enter cooldown per limited (Recommended: 0.75): ")
+    config["cooldownPerLimited"] = float(cooldownPerLimited)
+
+    jsonObject = json.dumps(config, indent=4)
+
+    with open("./config.json", "w") as f:
+        f.write(jsonObject)
+        f.close()
+    
+    print("Config file created. Restart the script.")
     input("Press enter to exit...")
     exit(0)
 
-    
-config["ROBLOSECURITY"] = roblosecurity
-
-cooldownPerLimited = input("Enter cooldown per limited (Recommended: 0.75): ")
-config["cooldownPerLimited"] = float(cooldownPerLimited)
-
-jsonObject = json.dumps(config, indent=4)
-
-with open("./config.json", "w") as f:
-    f.write(jsonObject)
+with open("./config.json", "r") as f:
+    print("Loading config...")
+    config = json.load(f)
     f.close()
+
+jsonObject = None
+print("What would you like to edit? \n"
+        "1. ROBLOSECURITY \n"
+        "2. Cooldown per limited \n"
+        "3. Exit")
+
+choice = int(input(""))
+
+if choice == 1:
+    roblosecurity = str(input("Enter ROBLOSECURITY: "))
+    try:
+        userId = r.get("https://users.roblox.com/v1/users/authenticated",
+                    cookies={".ROBLOSECURITY": roblosecurity}).json()["id"]
+    except:
+        print("ROBLOSECURITY is invalid. Restart the script and try again.")
+        input("Press enter to exit...")
+        exit(0)
+
+    config["ROBLOSECURITY"] = roblosecurity
+    jsonObject = json.dumps(config, indent=4)
+elif choice == 2:
+    cooldownPerLimited = input("Enter cooldown per limited (Recommended: 0.75): ")
+    config["cooldownPerLimited"] = float(cooldownPerLimited)
+    jsonObject = json.dumps(config, indent=4)  
+elif choice == 3:
+    exit(0)
+
+
+if jsonObject:
+    with open("./config.json", "w") as f:
+        f.write(jsonObject)
+        f.close()
+
+    print("Config file updated. Restart the script.")
+    input("Press enter to exit...")
+    exit(0)
