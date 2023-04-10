59f89118aa58574df26a71187b0ae10ace8d20e0
Noah <noahdevelopsthings@gmail.com>
2023-04-10 17:44:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ import datetime
 
 
 print("Checking for updates...")
-# script = r.get("https://raw.githubusercontent.com/noahrepublic/Better-UGC-Sniper/main/main.py").text
-# with open("main.py", "r") as f:
-   # if f.read() != script:
-       # print("Updating...")
-        # with open("main.py", "w") as f:
-            # f.write(script)
-            # input("Updated please reopen the script")
-            # exit(0)
+script = r.get("https://raw.githubusercontent.com/noahrepublic/Better-UGC-Sniper/main/main.py").text
+with open("main.py", "r") as f:
+   if f.read() != script:
+        print("Updating...")
+        with open("main.py", "w") as f:
+            f.write(script)
+            input("Updated please reopen the script")
+            exit(0)
 
 
 os.system("cls")
