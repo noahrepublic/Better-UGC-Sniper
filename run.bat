0a25a4e1a483d54625ac09c4cb7af8c2c9b9426c
Noah <noahdevelopsthings@gmail.com>
2023-04-10 19:06:13 -0300

    Fixed binds

--- run.bat
+++ run.bat
@@ -6,13 +6,13 @@ title UGC-Sniper Config Editor by noahrepublic#4323
 echo option select
 echo.
 echo [1] - Configure Sniper
-echo [2] - Add limited 
-echo [3] - Start Sniper
+
+echo [2] - Start Sniper
 
 set /p o=
 if %o%==1 goto config
-if %o%==2 goto add
-if %o%==3 goto start
+
+if %o%==2 goto start
 
 pause
 
@@ -20,6 +20,7 @@ pause
 cls
 python3 ./scripts/config.py
 goto ui
+
 
 
 :start
