1e4b81943ec5da4f290cc3a0f488e2ca32811a6c
Noah <noahdevelopsthings@gmail.com>
2023-04-11 17:25:36 -0300

    First time user donwloads

---
+++ scripts/startcheck.py
@@ -0,0 +1,8 @@
+import os
+
+if not os.path.exists("./config.json"):
+    print("Ensuring pip is installed")
+    os.system("python -m ensurepip --upgrade")
+
+    print("Installing required packages")
+    os.system("python -m pip install -r ./requirements.txt")
