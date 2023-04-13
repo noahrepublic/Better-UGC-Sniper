770c4e1d957b513d857be3bbc15304339060f65d
Noah <noahdevelopsthings@gmail.com>
2023-04-12 22:41:56 -0300

    Revert "Fixed tests"
    This reverts commit 745ed52b0c10ae12d6cd8243af91938b74df3ea3.

--- scripts/proxytest.py
+++ scripts/proxytest.py
@@ -6,6 +6,7 @@ import time
 import os
 
 proxies = queue.Queue()
+validProxies = []
 
 if not os.path.exists("./proxies.txt"):
     print("proxies.txt does not exist.")
@@ -25,7 +26,6 @@ with open("./proxies.txt", "r") as f:
 f = open("./proxies.txt", "w")
 
 startSize = proxies.qsize()
-print("Starting proxy validation with", startSize, "proxies.")
 runs = 0
 
 session = r.Session()
@@ -34,21 +34,26 @@ def validateProxy():
     global proxies
     global runs
     while not proxies.empty():
+        print("Progress:", f"{round((runs / startSize) * 100, 2)}%", end="\r")
         proxy = proxies.get()
         try:
             response = session.get("https://ipinfo.io/json", proxies={"https": proxy, "http": proxy}, timeout=5)
         except:
             runs += 1
-            print(f"{runs}/{startSize}")
             continue
-        
 
         if response.status_code == 200:
+            validProxies.append(proxy)
             f.write(f"\n{proxy}")
             runs += 1
-            print(f"{runs}/{startSize}")
         
 
     
 for _ in range(10):
     Thread(target=validateProxy).start()
+    
+while runs < startSize or not proxies.empty():
+    time.sleep(0.1)
+
+print("Done.")
+f.close()
