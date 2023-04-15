6633ebb7e1a682a094b0d7b56d2be28217dc9c84
Noah <noahdevelopsthings@gmail.com>
2023-04-15 16:11:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import queue
 
 os.system("cls")
 
-print("Better UGC Sniper by noahrepublic#4323, support server: https://discord.gg/hw2ttCnmdz")
+print("Better UGC Sniper by NoahDevelops#2502, support server: https://discord.gg/hw2ttCnmdz")
 print("https://github.com/noahrepublic/Better-UGC-Sniper")
 
 
